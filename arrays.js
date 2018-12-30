62c6d379f3a5eaa973fee34a237361e18d7a683c
Jeremiah  Acayturri <jtacayturri@gmail.com>
2018-12-30 23:24:01 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,6 +2,7 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 var candyString = "foo"
 
+
 function addElementToBeginningOfArray(chocolateBars, candyString) {
   return [candyString, ...chocolateBars];
 }
