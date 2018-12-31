ba334cc1733c4589951be556c022a1f277cc7b5b
Jeremiah  Acayturri <jtacayturri@gmail.com>
2018-12-31 00:31:18 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -41,7 +41,7 @@ function destructivelyRemoveElementFromEndOfArray(chocolateBars) {
 }
 
 function removeElementFromEndOfArray(chocolateBars) {
-  chocolateBars.slice(0, chocolateBars.length - 1);
+  chocolateBars.pop(0, chocolateBars.length - 1);
   return chocolateBars
 }
 
